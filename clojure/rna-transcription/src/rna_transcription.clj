b57adb8b9f1b2f70dbecb1a209f5c4ad0a6f0131
Robert Long <robert@trabian.com>
2017-05-24 00:02:09 -0400

    Add solution for rna_transcription

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,14 @@
-(ns rna-transcription)
+(ns rna-transcription
+	(:require [clojure.string :as string]))
 
-(defn to-rna [] ;; <- arglist goes here
+(defn isValidDna [possibleDna]
+	(string/blank? (string/replace possibleDna #"[GCTA]" ""))
+)
+
+(defn to-rna [dna] ;; <- arglist goes here
   ;; your code goes here
+  (cond
+  	(not (isValidDna dna)) (throw (AssertionError.))
+  	:else (string/replace (string/replace (string/replace (string/replace (string/replace dna "A" "U") "T" "A") "C" "H") "G" "C") "H" "G")
+  )
 )
