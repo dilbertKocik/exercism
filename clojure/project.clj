f02ffeb4baa871c9510415407cfdebd4c203432b
Robert Long <robert@trabian.com>
2017-06-07 13:52:37 -0400

    Add overall clojure project file

---
+++ clojure/project.clj
@@ -0,0 +1,4 @@
+(defproject exercism "0.1.0-SNAPSHOT"
+  :description "exercism exercises."
+  :url "https://github.com/dilbertKocik/exercism/clojure"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
